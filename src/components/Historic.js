983435397d2d8819d8fedf0b1fe204a4bfdb9a3b
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-05-30 20:01:27 -0300

    Init implement react calendar

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,14 +1,60 @@
+
+import styled from 'styled-components'
+import Calendar from "react-calendar"
+import "react-calendar/dist/Calendar.css";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import { useContext, useState, useEffect } from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
-import styled from 'styled-components'
+import UserContext from '../contexts/UserContext';
+import axios from 'axios';
 
-export default function Historic (){
-    return(
+export default function Historic() {
+
+    const { token } = useContext(UserContext)
+    const [historic, setHistoric] = useState([])
+
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
+
+    useEffect(() => {
+        const promisse = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily', config)
+        promisse.then(response => setHistoric(response.data))
+    }, [])
+
+    function formatDay(date) {
+
+        let teste = dayjs(date).locale('pt-br').format("DD")
+
+        for (let i = 0; i < historic.length; i++) {
+
+            let dayHistoric = (historic[i].day).substring(0,2)
+
+            if (dayjs(date).locale('pt-br').format("DD") === dayHistoric){
+                historic[i].habits.map((value) => {
+                    
+                    if(value.done === false){
+                        teste= <span>NESSE DIA FALHOU</span>;
+                    }
+                
+                })
+
+            }
+        
+    }  return(teste)}
+
+
+    return (
         <Container>
-            <Header/>
+            <Header />
             <h3>Histórico</h3>
-            <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
-            <Footer/>
+            <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p><br />
+            <Calendar calendarType="Hebrew" formatDay={(locale, date) => formatDay(date)} />
+            <Footer />
         </Container>
     )
 }
@@ -25,5 +71,9 @@ const Container = styled.div`
         color: var(--header-color);
         margin-bottom: 20px;
         font-size: 1.44rem;
+    }
+
+    span{
+        color: red;
     }
 `
